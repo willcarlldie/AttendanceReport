5c1332f31534c62223287e4b268789245eb9f69e
Tim <tim@tim-ings.com>
2017-08-20 04:35:33 +0800

    Adds table output

--- report/js/report.js
+++ report/js/report.js
@@ -29,6 +29,70 @@ var playerClasses = [
     "Warrior"
 ];
 var players = [];
+var reportsParsed = 0;
+var reportsTotal = 0;
+
+function updateProgBar() {
+    var pb = $("#prog-parse");
+    pb.attr("style", "width:" + reportsParsed / reportsTotal * 100 + "%");
+    pb.text(reportsParsed + " / " + reportsTotal);
+}
+
+function tryOutputReport() {
+    // dont output an incomplete report
+    if (reportsParsed != reportsTotal) {
+        return;
+    }
+    console.log("Outputting attendance report for " + players.length + " players");
+
+    var weekTable = [];
+    for (var i = 0; i < players.length; i++) {
+        var player = players[i];
+        var dayRates = [];
+        for (var j = 0; j < 7; j++) {
+            dayRates[j] = (Math.round(player.presentDays[j] / player.totalDays[j] * 100) || 0) + "%";
+        }
+        var row = {
+            "name": player.name,
+            "monday": dayRates[0],
+            "tuesday": dayRates[1],
+            "wednesday": dayRates[2],
+            "thursday": dayRates[3],
+            "friday": dayRates[4],
+            "saturday": dayRates[5],
+            "sunday": dayRates[6]
+        };
+        weekTable.push(row);
+    }
+    $("#tb-weekday-rates").bootstrapTable({
+        columns: [{
+            field: "name",
+            title: "Name"
+        }, {
+            field: "monday",
+            title: "Monday"
+        }, {
+            field: "tuesday",
+            title: "Tuesday"
+        }, {
+            field: "wednesday",
+            title: "Wednesday"
+        }, {
+            field: "thursday",
+            title: "Thursday"
+        }, {
+            field: "friday",
+            title: "Friday"
+        }, {
+            field: "saturday",
+            title: "Saturday"
+        }, {
+            field: "sunday",
+            title: "Sunday"
+        }],
+        data: weekTable
+    });
+}
 
 function discoverPlayers(report) {
     for (var i = 0; i < report.friendlies.length; i++) {
@@ -43,20 +107,79 @@ function discoverPlayers(report) {
                 }
             }
             if (!alreadyInSet) {
+                friend.presentReports = [];
+                friend.lateReports = [];
+                friend.earlyLeaveReports = [];
+                friend.absentReports = [];
+                friend.presentDays = [0, 0, 0, 0, 0, 0, 0, 0];
+                friend.lateDays = [0, 0, 0, 0, 0, 0, 0, 0];
+                friend.earlyLeaveDays = [0, 0, 0, 0, 0, 0, 0, 0];
+                friend.absentDays = [0, 0, 0, 0, 0, 0, 0, 0];
+                friend.totalDays = [0, 0, 0, 0, 0, 0, 0, 0];
                 players.push(friend);
             }
         }
     }
 }
 
+function playerWasLate(player) {
+    return player.fights.length > 0 &&
+        player.fights[0].id != 1;
+}
+
+function playerLeftEarly(player, report) {
+    return player.fights.length > 0 &&
+        report.fights.length > 0 &&
+        player.fights[player.fights.length - 1].id != report.fights[report.fights.length - 1].id;
+}
+
 function parseReport(report) {
+    // parse the report
     console.log("Parsing report: " + report.title);
+    // add unique players to our player list
     discoverPlayers(report);
+    for (var i = 0; i < players.length; i++) {
+        var player = players[i];
+        playerInReport = false;
+        startDate = new Date(report.start);
+        // keep track of how many reports we see on each given week day
+        player.totalDays[startDate.getDay()]++;
+        // try and find the player in the report
+        for (var j = 0; j < report.friendlies.length; j++) {
+            var friend = report.friendlies[j];
+            if (friend.guid == player.guid) {
+                // keep track of the reports the player is in
+                playerInReport = true;
+                player.presentReports.push(report);
+                player.presentDays[startDate.getDay()]++;
+                // check if the player was late
+                if (playerWasLate(player)) {
+                    player.lateReports.push(report);
+                    player.lateDays[startDate.getDay()]++;
+                }
+                // check if the player left early
+                if (playerLeftEarly(player, report)) {
+                    player.earlyLeaveReports.push(report);
+                    player.earlyLeaveDays[startDate.getDay()]++;
+                }
+            }
+            // keep track of the reports the player missed
+            if (!playerInReport) {
+                player.absentReports.push(report);
+                player.absentDays[startDate.getDay()]++;
+            }
+        }
+    }
+    reportsParsed++;
+    updateProgBar();
+    tryOutputReport();
 }
 
 function fetchFights(reports) {
+    // fetch each individual report
     console.log("Received " + reports.length + " reports.");
     for (var i = 0; i < reports.length; i++) {
+        // check if the report is in a valid zone
         var report = reports[i];
         var validZone = false;
         for (var zone in zones) {
@@ -66,6 +189,8 @@ function fetchFights(reports) {
             }
         }
         if (validZone) {
+            // fetch the report
+            reportsTotal++;
             console.log("Fetching report: " + report.id);
             var request_url = "https://www.warcraftlogs.com:443/v1/report/fights/" + report.id + "?api_key=" + wclkey;
             $.get(request_url, function (reportData) {
@@ -76,6 +201,11 @@ function fetchFights(reports) {
 }
 
 function generateReport() {
+    // reset counters
+    reportsParsed = 0;
+    reportsTotal = 0;
+
+    // gather input
     guild = $("#inp-guild").val();
     realm = $("#inp-realm").val();
     region = $("#inp-region").val();
@@ -85,6 +215,7 @@ function generateReport() {
     zones["12"] = $("#inp-zone12").is(":checked");
     zones["13"] = $("#inp-zone13").is(":checked");
 
+    // request wcl reports for given guild
     console.log("Fetching reports for: " + region + "/" + realm + "/" + guild);
     var request_url = "https://www.warcraftlogs.com:443/v1/reports/guild/" + guild + "/" + realm + "/" + region + "?api_key=" + wclkey;
     $.ajax({
@@ -101,12 +232,6 @@ function generateReport() {
             404: function () {
                 alert("Error 404: Unable to find reports for the given guild/realm/region");
             }
-        },
+        }
     });
 }
-
-$(function () {
-    /*$("#table").bootstrapTable({
-        data: data
-    });*/
-});
